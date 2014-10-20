eba6b8d7718685b9a05aeb127ad1b50e90bb342a
Bleskino <martin.kysel.mk@gmail.com>
2014-10-20 16:58:32 +0200

    treti commit

--- app/src/main/java/emkej/rssianalyzer/Analyzer.java
+++ app/src/main/java/emkej/rssianalyzer/Analyzer.java
@@ -1,6 +1,14 @@
 package emkej.rssianalyzer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.lang.Object;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -9,9 +17,15 @@ import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 
 public class Analyzer extends Activity {
@@ -20,7 +34,7 @@ public class Analyzer extends Activity {
     WifiReceiver receiverWifi;
     List<ScanResult> wifiList;
     StringBuilder sb = new StringBuilder();
-
+    Button btn1,btn2;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +44,37 @@ public class Analyzer extends Activity {
         receiverWifi = new WifiReceiver();
         registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
         mainWifi.startScan();
-        // save test
+
+        btn2=(Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                 mainWifi.startScan();
+                 }
+                 });
+
+        btn1= (Button)findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        File myFile = new File(Environment.getExternalStorageDirectory().getPath() + "/merania.txt");
+                        myFile.createNewFile();
+                        FileOutputStream fOut = new FileOutputStream(myFile);
+                        OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+                        myOutWriter.append(sb);
+                        myOutWriter.close();
+                        fOut.close();
+                            Toast.makeText(getBaseContext(), "Dáta uložené", Toast.LENGTH_LONG).show();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
     }
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         menu.add("Obnoviť");
@@ -40,7 +83,7 @@ public class Analyzer extends Activity {
 
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
         mainWifi.startScan();
-        return super.onMenuItemSelected(featureId,item);
+        return super.onMenuItemSelected(featureId, item);
     }
 
 
@@ -57,14 +100,22 @@ public class Analyzer extends Activity {
     class WifiReceiver extends BroadcastReceiver {
         public void onReceive(Context context, Intent intent) {
 
-            sb = new StringBuilder();
+            //sb = new StringBuilder(); //ak chcem uchovat predch. zapis
             wifiList = mainWifi.getScanResults();
             for(int i = 0; i < wifiList.size(); i++){
-                sb.append((wifiList.get(i)).toString() + "\n\n");
-
+                sb.append("SSID: " + (wifiList.get(i).SSID) + "\n");
+                sb.append("AP MAC: " + (wifiList.get(i).BSSID) + "\n");
+                sb.append("ZABEZPEČENIE: " + (wifiList.get(i).capabilities) + "\n");
+                sb.append("FREKVENCIA: " + (wifiList.get(i).frequency) + " Hz \n");
+                sb.append("RSSI: " + (wifiList.get(i).level) + "dBm \n");
+                //timestamp je až od API 17
+                sb.append("\n\n");
             }
             mainText.setText(sb);
 
         }
+
     }
+
+
 }
